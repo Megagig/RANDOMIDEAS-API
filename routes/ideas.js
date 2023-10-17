6ec6ea9aec92e1b13b9b368ce7a3485775e7ae14
megagig <megagigsoftwaresolutions@gmail.com>
2023-10-17 18:42:10 +0100

    Install Nodemon and Clean up the server.js

---
+++ routes/ideas.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const router = express.Router();
+
+//Generate fake data
+const ideas = [
+  {
+    id: 1,
+    text: 'Positive Newsletter, a newsletter that only shares positive, uplifting news',
+    tag: 'Technology',
+    username: 'TonyStark',
+    date: '2022-01-02',
+  },
+  {
+    id: 2,
+    text: 'Milk cartons that turn a different color the older your milk is getting',
+    tag: 'Inventions',
+    username: 'SteveRogers',
+    date: '2022-01-02',
+  },
+  {
+    id: 3,
+    text: 'ATM location that allows you to withdraw money from your bank account',
+    tag: 'Software',
+    username: 'BruceWayne',
+    date: '2022-01-02',
+  },
+];
+//get all ideas
+router.get('/', (req, res) => {
+  res.json({ success: true, data: ideas });
+});
+//get single idea
+router.get('/:id', (req, res) => {
+  const idea = ideas.find((idea) => idea.id === +req.params.id);
+  if (!idea) {
+    return res
+      .status(404)
+      .json({ success: false, error: 'Resource not found' });
+  }
+  res.json({ success: true, data: idea });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,50 +2,13 @@ const express = require('express');
 const port = 5000;
 const app = express();
 
-//Generate fake data
-const ideas = [
-  {
-    id: 1,
-    text: 'Positive Newsletter, a newsletter that only shares positive, uplifting news',
-    tag: 'Technology',
-    username: 'TonyStark',
-    date: '2022-01-02',
-  },
-  {
-    id: 2,
-    text: 'Milk cartons that turn a different color the older your milk is getting',
-    tag: 'Inventions',
-    username: 'SteveRogers',
-    date: '2022-01-02',
-  },
-  {
-    id: 3,
-    text: 'ATM location that allows you to withdraw money from your bank account',
-    tag: 'Software',
-    username: 'BruceWayne',
-    date: '2022-01-02',
-  },
-];
-
 //create Routes
 app.get('/', (req, res) => {
   res.json({
     message: 'Welcome to Random Ideas API!',
   });
 });
-//get all ideas
-app.get('/api/ideas', (req, res) => {
-  res.json({ success: true, data: ideas });
-});
-//get single idea
-app.get('/api/ideas/:id', (req, res) => {
-  const idea = ideas.find((idea) => idea.id === +req.params.id);
-  if (!idea) {
-    return res
-      .status(404)
-      .json({ success: false, error: 'Resource not found' });
-  }
-  res.json({ success: true, data: idea });
-});
 
+const ideasRouter = require('./routes/ideas');
+app.use('/api/ideas', ideasRouter);
 app.listen(port, () => console.log(`Server Listening on port ${port}...`));
